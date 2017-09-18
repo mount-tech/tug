f8575ca01d0f5f22b361a951fa9411a9e3072e9a
shockham <samuel.hockham@gmail.com>
2017-09-18 22:27:00 +0100

    Serves static files, with configurable root

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,14 @@ extern crate serde_derive;
 
 use futures::future::FutureResult;
 
-use hyper::{Get, Post, StatusCode};
+use hyper::StatusCode;
 use hyper::header::ContentLength;
 use hyper::server::{Http, Service, Request, Response};
 
 use std::thread;
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
 
 /// Main config
 #[derive(Debug, Deserialize)]
@@ -34,11 +37,15 @@ struct Config {
 #[derive(Debug, Deserialize)]
 struct ServerConfig {
     host: Option<String>,
+    root: Option<String>,
 }
 
 
 /// Empty struct for the Tug service
-struct Tug;
+struct Tug {
+    root: String,
+}
+
 
 /// Tug service implementation
 impl Service for Tug {
@@ -48,20 +55,20 @@ impl Service for Tug {
     type Future = FutureResult<Response, hyper::Error>;
 
     fn call(&self, req: Request) -> Self::Future {
-        futures::future::ok(match (req.method(), req.path()) {
-            (&Get, "/") => {
-                let test: &'static [u8] = b"test get";
-                Response::new()
-                    .with_header(ContentLength(test.len() as u64))
-                    .with_body(test)
-            }
-            (&Post, "/") => {
-                let test: &'static [u8] = b"test post";
-                Response::new()
-                    .with_header(ContentLength(test.len() as u64))
-                    .with_body(test)
-            }
-            _ => Response::new().with_status(StatusCode::NotFound),
+        let root_path = self.root.clone();
+        let path_string = format!("{}{}", root_path, req.path());
+        let file_path = Path::new(&path_string);
+
+        futures::future::ok(if file_path.exists() {
+            let mut file = File::open(file_path).unwrap();
+            let mut buf = Vec::new();
+            let _ = file.read_to_end(&mut buf);
+
+            Response::new()
+                .with_header(ContentLength(buf.len() as u64))
+                .with_body(buf)
+        } else {
+            Response::new().with_status(StatusCode::NotFound)
         })
     }
 }
@@ -75,6 +82,7 @@ fn main() {
 
         [[server]]
         host = "127.0.0.1:1337"
+        root = "./src"
     "#;
 
     let config: Config = toml::from_str(toml_str).unwrap();
@@ -82,9 +90,15 @@ fn main() {
     for server_config in config.server.unwrap() {
         let host = server_config.host.unwrap();
         let addr = host.parse().unwrap();
+        let root = server_config.root.unwrap_or(".".to_string());
 
         thread::spawn(move || {
-            let server = Http::new().bind(&addr, || Ok(Tug)).unwrap();
+            let server = Http::new()
+                .bind(&addr, move || {
+                    let tug = Tug { root: root.clone() };
+                    Ok(tug)
+                })
+                .unwrap();
             info!("Serving at http://{}", server.local_addr().unwrap());
             server.run().unwrap();
         });
